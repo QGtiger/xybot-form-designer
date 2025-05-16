2914c95a0a799ccc7f9d16c5117697fda72ecc9f
光鱼 <guang.yu@yingdao.com>
2025-05-16 16:18:57 +0800

    feat: 添加 base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "development" ? "/" : "/xybot-form-designer/",
   plugins: [react(), tailwindcss()],
   build: {
     minify: false,
